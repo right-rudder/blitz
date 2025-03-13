224cdfd468c8935e076413458b63ba3ea4864970
Diego Yon <diegoyon@outlook.com>
2025-03-12 22:21:30 -0500

    add navbar

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const FACEBOOK_URL = "";
+export const INSTAGRAM_URL = "";
+export const PHONE_NUMBER = "";
+export const EMAIL_ADDRESS = "";
+export const TIKTOK_URL = "";
+export const YOUTUBE_URL = "";
---
+++ src/data/navbarLinks.js
@@ -0,0 +1,70 @@
+export const navbarLinks = [
+  {
+    name: "First-Time Pilots",
+    link: "/first-time-pilots",
+    submenu: [],
+  },
+  {
+    name: "Programs",
+    submenu: [
+      {
+        name: "Private Pilot",
+        link: "/programs/private-pilot",
+      },
+      {
+        name: "Instrument Rating",
+        link: "/programs/instrument-rating",
+      },
+      {
+        name: "Commercial Pilot",
+        link: "/programs/commercial-pilot",
+      },
+      {
+        name: "Multi-Engine Rating",
+        link: "/programs/multi-engine-rating",
+      },
+      {
+        name: "Certified Flight Instructor",
+        link: "/programs/cfi",
+      },
+      {
+        name: "Helicopter Pilot Training",
+        link: "/programs/helicopter-pilot-training",
+      },
+    ],
+  },
+  {
+    name: "News",
+    link: "/news",
+  },
+  {
+    name: "About Us",
+    submenu: [
+      {
+        name: "Our Story",
+        link: "/about/our-story",
+      },
+      {
+        name: "Our Team",
+        link: "/about/our-team",
+      },
+      {
+        name: "Our Fixed-Wing Fleet",
+        link: "/about/our-fixed-wing-fleet",
+      },
+      {
+        name: "Our Rotary-Wing Fleet",
+        link: "/about/our-rotary-wing-fleet",
+      },
+      {
+        name: "Our Simulators",
+        link: "/about/our-simulators",
+      },
+    ],
+  },
+  {
+    name: "Contact Us",
+    link: "/contact",
+    submenu: [],
+  },
+];

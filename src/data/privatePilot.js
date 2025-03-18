ac050348d8ecc93a73ca116147f5924079a32be3
Diego Yon <diegoyon@outlook.com>
2025-03-18 14:51:45 -0500

    minor changes

--- src/data/privatePilot.js
+++ src/data/privatePilot.js
@@ -10,7 +10,7 @@ const privatePilot = {
     siteTitle: "Private Pilot | Blitz Aviation",
     siteDescription:
       "Blitz Aviation believes that anyone with a passion for flying can become a pilot with the right training, dedication, and support. Watch our video by Bill Macon that outlines the three main tests you need to pass to obtain your pilot's license: the medical exam, written test, and flight test (check ride). We believe that the key to your success lies in our commitment to providing the highest quality flight training available, with a focus on safety, professionalism, and individual attention.",
-    primaryImage: "/src/assets/images/pexels-tracvu-5741643.jpg",
+    primaryImage: "/src/assets/images/cfi-cambrie-foster.jpg",
     primaryAlt: "Pilot flying on IFR flight",
     intro:
       "The Private Pilot License (PPL) is your first step toward professional training and qualifies you to fly for personal or recreational purposes. At Blitz Aviation, our Private Pilot course integrates flight training and ground instruction through a structured syllabus, accelerating your learning and laying the foundation for future advanced ratings and certifications.",

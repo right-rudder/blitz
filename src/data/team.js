00e615d872d9203048ab3fea3349c73876cda817
Diego Yon <diegoyon@outlook.com>
2025-03-27 12:08:18 -0500

    add team member

--- src/data/team.js
+++ src/data/team.js
@@ -4,12 +4,27 @@ const team = [
     certificatesAndRatings: ["CFI", "CFII"],
     image: "/src/assets/images/mycah-pittman.webp",
     bio: "Mycah earned his ratings in just 10 months—all while balancing life as a student-athlete. If you're looking to get your Private Pilot, Instrument, or Commercial License fast and efficiently, he's the instructor for you!",
+    facebook: "https://www.facebook.com/mycah.pittman",
+    instagram: "https://www.instagram.com/mycahpittman/?hl=en",
+    x: "https://x.com/mycahpittman?lang=en",
   },
   {
     name: "Andrew Zilka",
     image: "/src/assets/images/andrew-zilka.webp",
     certificatesAndRatings: ["CFI", "CFII", "MEI"],
     bio: "With a strong family history in aviation and a passion for teaching, he’s a great addition to our team. Whether you're starting your flight training or advancing your skills, Andrew is here to guide you to success.",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/up_the_andy/",
+    x: "https://x.com",
+  },
+  {
+    name: "Cambrie Foster",
+    image: "/src/assets/images/cambrie-foster.webp",
+    certificatesAndRatings: ["CFI", "CFII", "AGI"],
+    bio: "Cambrie is excited to share her passion for aviation with you. She is a great instructor for students looking to earn their Private Pilot or Instrument Rating.",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/flying.foster/",
+    x: "https://x.com",
   },
 ];
 

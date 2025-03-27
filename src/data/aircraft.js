8c7c81bda8e4f4e100ead45c5d9e3f96930b1493
Diego Yon <diegoyon@outlook.com>
2025-03-26 23:52:15 -0500

    add our fleet

---
+++ src/data/aircraft.js
@@ -0,0 +1,34 @@
+const aircraft = [
+  {
+    name: "Cessna 172XP",
+    number: "N122XP",
+    image1: "/src/assets/images/cessna-172xp.webp",
+    image1Alt: "Cessna 172XP",
+    image2: "/src/assets/images/pexels-csanq.webp",
+    image2Alt: "Control Panel of Cessna 172XP",
+    image3: "/src/assets/images/cessna-172xp-at-blitz-aviation.webp",
+    image3Alt: "Cessna 172XP at Blitz Aviation",
+    description:
+      "The Cessna 172XP is a versatile and reliable aircraft that is perfect for flight training, sightseeing, and cross-country flights. With its advanced avionics and comfortable cabin, the 172XP is a popular choice among pilots of all experience levels.",
+    equipment: [
+      "GFC 500 Autopilot (TAA)",
+      "Garmin G530 WAAS",
+      "Dual Garmin G5s",
+    ],
+  },
+  {
+    name: "Cessna 172P",
+    number: "N99832",
+    image1: "/src/assets/images/cessna-172p.webp",
+    image1Alt: "Cessna 172P",
+    image2: "/src/assets/images/pexels-captainsopon.webp",
+    image2Alt: "Control Panel of Cessna 172P",
+    image3: "/src/assets/images/cessna-172p-at-blitz-aviation.webp",
+    image3Alt: "Cessna 172P at Blitz Aviation",
+    description:
+      "The Cessna 172P is a classic trainer that has been used by generations of pilots to earn their wings. With its simple design, reliable performance, and forgiving flight characteristics, the 172P is an ideal aircraft for student pilots and experienced aviators alike.",
+    equipment: ["Dual Garmin G5", "Garmin 625 GPS", "Climb Prop"],
+  },
+];
+
+export default aircraft;

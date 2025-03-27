d489b6594278e5136ec3d9ea35edb8d8d05370db
Diego Yon <diegoyon@outlook.com>
2025-03-26 22:49:36 -0500

    add our team page

---
+++ src/data/team.js
@@ -0,0 +1,16 @@
+const team = [
+  {
+    name: "Mycah Pittman",
+    certificatesAndRatings: ["CFI", "CFII"],
+    image: "/src/assets/images/mycah-pittman.webp",
+    bio: "Mycah earned his ratings in just 10 months—all while balancing life as a student-athlete. If you're looking to get your Private Pilot, Instrument, or Commercial License fast and efficiently, he's the instructor for you!",
+  },
+  {
+    name: "Andrew Zilka",
+    image: "/src/assets/images/andrew-zilka.webp",
+    certificatesAndRatings: ["CFI", "CFII", "MEI"],
+    bio: "With a strong family history in aviation and a passion for teaching, he’s a great addition to our team. Whether you're starting your flight training or advancing your skills, Andrew is here to guide you to success.",
+  },
+];
+
+export default team;

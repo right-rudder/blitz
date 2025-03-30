49564d286446f4d18824d983885b8d0a3bf6e888
Diego Yon <diegoyon@outlook.com>
2025-03-30 16:29:22 -0500

    set up discovery flight links

--- src/data/services.js
+++ src/data/services.js
@@ -6,6 +6,7 @@ const services = [
     title: "Take a Discovery Flight",
     description:
       "The best way to experience flying is to take a Discovery Flight. This short introductory flight offers you the chance to fly with one of our experienced instructors. You'll take the controls, learn basic maneuvers, and enjoy breathtaking views of Salt Lake City and the surrounding area.",
+    url: "discovery-flight",
   },
   {
     imageSrc: "/src/assets/images/pexels-tranthangnhat-17594303.jpg",
@@ -13,6 +14,7 @@ const services = [
     title: "Introductory Flight Lessons",
     description:
       "Our Introductory Flight Lessons are the perfect way to begin your journey to becoming a pilot. You'll learn the basics of flying, experience the thrill of taking the controls, and get a taste of what it's like to fly through the skies. Our experienced instructors will guide you through each step, ensuring that you have a safe and enjoyable flight.",
+    url: "programs/private-pilot",
   },
   {
     imageSrc: "/src/assets/images/pexels-felix-57767809-29170959.jpg",
@@ -20,6 +22,7 @@ const services = [
     title: "Advanced Flight Training",
     description:
       "For pilots looking to advance their skills, we offer a range of advanced flight training programs. Whether you're pursuing your instrument rating, commercial pilot certificate, or multi-engine rating, our experienced instructors will provide you with the knowledge and skills you need to succeed. With our aircraft and comprehensive training curriculum, you'll be well-prepared to take your flying to the next level.",
+    url: "#",
   },
   {
     imageSrc: "/src/assets/images/pexels-kokorevas-5313876.jpg",
@@ -27,6 +30,7 @@ const services = [
     title: "Aircraft Rental",
     description:
       "Blitz Aviation offers a wide range of aircraft for rent, from single-engine trainers to multi-engine aircraft. Whether you're looking to build flight hours, explore new destinations, or simply enjoy the freedom of flight, we have the perfect aircraft for your needs. Our rental fleet is well-maintained and equipped with the latest avionics.",
+    url: "about/our-fleet",
   },
   {
     imageSrc: "/src/assets/images/pexels-josephmartin-5785741.jpg",
@@ -34,6 +38,7 @@ const services = [
     title: "Aviation Career Training",
     description:
       "For aspiring pilots looking to turn their passion for flying into a career, we offer comprehensive aviation career training programs. From private pilot to certified flight instructor, our training programs are designed to provide you with the knowledge and skills needed to succeed in the dynamic field of aviation.",
+    url: "#trainingPrograms",
   },
   {
     imageSrc: "/src/assets/images/pexels-junerydocto-15543665.jpg",
@@ -41,6 +46,7 @@ const services = [
     title: "Flight School Resources",
     description:
       "Our website offers a wealth of resources for aspiring pilots including flight school information, financing options, study materials, career guidance, and more. Whether you're just starting your flight training or looking to advance your career, our resources are designed to help you make informed decisions and achieve your aviation goals.",
+    url: "blog",
   },
 ];
 

bbfda57444aad2fd464e476955859b1e29845408
Diego Yon <diegoyon@outlook.com>
2025-07-23 12:16:34 -0500

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
-
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blitzaviation.com/",
-  integrations: [mdx(), sitemap(), react(), partytown()],
+  integrations: [mdx(), sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],

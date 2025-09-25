5aa373dc649bdadede0724bf5fb44c710a977419
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-09-25 00:55:39 -0300

    Create CheckrideStudentsCTA.js

---
+++ src/data/CheckrideStudentsCTA.js
@@ -0,0 +1,11 @@
+const cta = {
+  title: "Ready to Start Your Flight Training?",
+  description:
+    "Take the first step toward earning your Private Pilot License with Blitz Aviation. Contact us today to schedule your introductory flight and begin your journey in aviation!",
+  link: "/enrollment-form",
+  linkText: "Enroll Now",
+  imagePath: "/src/assets/images/Student First Solo Ethan Smith.webp",
+  imageAlt: "Student with CFI after first solo flight",
+};
+
+export default cta;

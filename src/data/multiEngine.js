69dee0425a658115241a4375827bf729969ebb56
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2025-11-24 17:13:20 -0300

    Update multi-engine images and add a small callout for a new course

--- src/data/multiEngine.js
+++ src/data/multiEngine.js
@@ -11,17 +11,39 @@ const multiEngine = {
     siteDescription:
       "Upgrade your pilot qualifications with multi-engine training at Blitz Aviation in Ogden, Utah. Master twin-engine operations and engine-out procedures with our experienced instructors in a comprehensive, safety-focused program.",
     siteKeywords:
-      "multi-engine rating Utah, twin engine training, Piper Seminole training, MEL rating Ogden, engine-out procedures training, multi-engine commercial pilot, twin engine qualification, multi-engine checkride preparation, advanced pilot certification, twin aircraft flight training, multi-engine land rating",
+      "multi-engine rating Utah, twin engine training, Beechcraft 55 Baron training, MEL rating Ogden, engine-out procedures training, multi-engine commercial pilot, twin engine qualification, multi-engine checkride preparation, advanced pilot certification, twin aircraft flight training, multi-engine land rating",
     intro:
       "A Multi-Engine Rating qualifies you to fly multi-engine aircraft, a prerequisite for most commercial airline roles, and prepares you to handle complex scenarios like engine failures. At Blitz Aviation, our multi-engine training emphasizes precision, safety, and operational excellence, enhancing your credentials and expanding career opportunities in airline operations.",
     description: [
+      `<span class="block bg-main-blue-100 p-5">
+        <span class="block mb-4">
+          <span class="text-blue-800 font-semibold text-2xl tracking-wide">Exciting news!</span>
+          <span class="text-blue-800 font-normal text-xl">Blitz is finally getting a multi!</span>
+        </span>
+
+        <span class="block mb-4">
+          We're offering a 7-10 day multi engine course for $5,900, checkride fee not included. This course will get you 10 hrs in aircraft and 15-20 hrs with instructor, with a checkride <b>after you've been signed off</b>.
+        </span>
+
+        <span class="flex items-center gap-2 font-normal"><span class="inline-block size-1.5 rounded-full bg-black"></span> Guaranteed checkride.</span>
+        <span class="flex items-center gap-2 font-normal"><span class="inline-block size-1.5 rounded-full bg-black"></span> Commercial add on.</span>
+        <span class="flex items-center gap-2 font-normal"><span class="inline-block size-1.5 rounded-full bg-black"></span> MEI.</span>
+
+        <span class="block mt-4">
+          Fill out the form below or call us directly at <a href="tel:(801)259-8009" class="text-link">(801) 259-8009</a>.
+        </span>
+
+        <a href="#contactUs" class="btn-primary w-fit mt-4">Get on Our Schedule</a>
+      </span>`,
       "The Multi-Engine Rating is a valuable qualification that allows you to fly multi-engine aircraft, opening up new opportunities in commercial aviation. Whether you're looking to pursue a career with the airlines, fly charter flights, or enhance your piloting skills, the Multi-Engine Rating is an essential step in your journey. At Blitz Aviation, our comprehensive program provides you with the knowledge and skills needed to excel as a multi-engine pilot.",
       "Our Multi-Engine Rating program is designed to deepen your understanding of multi-engine flight and enhance your proficiency in complex aircraft operations. With a focus on precision and safety, our program prepares you to handle the challenges of multi-engine flight, including engine failures and emergency procedures. By combining expert instruction with hands-on experience, we'll help you become a skilled and confident multi-engine pilot.",
     ],
-    primaryImage: "/src/assets/images/twin-comanche.jpeg",
-    primaryAlt: "Multi engine instructor pass checkride",
-    secondaryImage: "/src/assets/images/cfi-spencer-rowberry.jpeg",
-    secondaryAlt: "Cessna 172XP control panel",
+    primaryImage:
+      "/src/assets/images/front-view-beechcraft-55-baron-N8145R-blitz-aviation-utah-multi-engine-rating.jpg",
+    primaryAlt: "Front view of a Beechcraft 55 Baron multi engine aircraft",
+    secondaryImage:
+      "/src/assets/images/front-view-beechcraft-55-baron-N8145R-blitz-aviation-utah-multi-engine-rating.jpg",
+    secondaryAlt: "Front view of a Beechcraft 55 Baron multi engine aircraft",
     prerequisites: [
       "Hold a current private pilot certificate",
       "Hold a valid FAA 3rd Class Medical Certificate or a higher class",
@@ -32,7 +54,7 @@ const multiEngine = {
         title: "Complete Ground School and Flight Training",
         icon: FaFileMedical,
         description:
-          "Our multi engine training program begins with a comprehensive ground school that covers the principles of multi engine flight, systems, and procedures. You'll learn about the unique characteristics of multi engine aircraft and the critical considerations for safe operation. With our expert instructors, you'll then take to the skies in our Piper Seminole, where you'll gain hands-on experience with twin engines and advanced systems. Through a structured blend of flight training, you'll master multi engine operations and develop the skills needed to confidently pilot multi engine aircraft.",
+          "Our multi engine training program begins with a comprehensive ground school that covers the principles of multi engine flight, systems, and procedures. You'll learn about the unique characteristics of multi engine aircraft and the critical considerations for safe operation. With our expert instructors, you'll then take to the skies in our Beechcraft Baron, where you'll gain hands-on experience with twin engines and advanced systems. Through a structured blend of flight training, you'll master multi engine operations and develop the skills needed to confidently pilot multi engine aircraft.",
       },
       {
         title: "Accumulate Flight Time",
